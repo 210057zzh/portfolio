62ed3ed8b8e9f82c3730f8fca484542ca9c53c45
David Zhang <zhazhih@amazon.com>
2023-06-08 13:04:22 -0700

    add time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Text, VStack } from "@chakra-ui/react";
 import About from "@/layout/sections/about";
+import Experience from "@/layout/sections/experience";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <VStack>
       <About></About>
+      <Experience></Experience>
     </VStack>
   );
 }
