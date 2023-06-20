e934ac0861a6a15dee7cfbfb26e18b5d88d043f1
David Zhang <210057zzh@gmail.com>
2023-06-19 20:45:09 -0700

    Deploy

---
+++ _next/static/iPvCKteVXA3jvBrLJjH1d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ae51ba48-ef5c3b837f6f87b6.js","static/chunks/140-c8238a44d8d13e8f.js","static/chunks/pages/index-c1d6f375d93b4b60.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
